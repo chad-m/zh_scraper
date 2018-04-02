04c1a2bc816ae88559988ad9ed0bad01625e24de
chad-m <chad39@gmail.com>
2018-04-01 20:47:26 -0500

    updates etl

--- etl/zh_dataframe_prep.py
+++ etl/zh_dataframe_prep.py
@@ -2,27 +2,39 @@
 # -*- coding: utf-8 -*-
 
 # Script to transform processed articles into dataframe and output as CSV
+
+
 import logging
-import pandas as pd
 import json
+
+import pandas as pd
+from textblob import TextBlob
+
 
 # Set up logging
 logger = logging.getLogger(__name__)
 
+
+# ==============
+# Load Data
+# ==============
 # Load processed articles data
 with open("../data/processed/processed_articles", "r") as f:
     articles_raw = f.read().split("\n")
     articles = []  # list of article dictionaries
-    for article in articles:
+    for article in articles_raw:
         try:
-            articles.append(article)
+            articles.append(json.loads(article))
         except Exception as e:
             logger.info("Could not process article")
             pass
 
 
-# Create dataframe
+# Create article dataframe
 article_df = pd.DataFrame.from_records(articles)
 
-print(article_df.info(), "\n"*4)
-print(article_df.head())
+
+# =====================
+# Feature Engineering
+# =====================
+# 
