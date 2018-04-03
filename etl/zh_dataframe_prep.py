70ec154ec141b878751f3279bdeb1c3e6bd638a6
chad-m <chad39@gmail.com>
2018-04-02 23:24:24 -0500

    updates feature engineering

--- etl/zh_dataframe_prep.py
+++ etl/zh_dataframe_prep.py
@@ -45,6 +45,12 @@ def main(input_file_path=INPUT_FILE, output_file_path=OUT_FILE):
     # Article title subjectivity
     article_df["article_title_subjectivity"] = article_title_sentiment.map(lambda _: _[1])
 
+    # Scale view count size for bubble chart
+    avc_max = article_df['article_views_count'].max()
+    avc_min = article_df['article_views_count'].min()
+    scale_factor = 2000
+    article_df["article_views_count_scaled"] = pd.np.sqrt(((article_df['article_views_count'] - avc_min) / (avc_max - avc_min)) * scale_factor) + 2
+
     # Output to csv
     article_df.to_csv(output_file_path, index=False)
 
